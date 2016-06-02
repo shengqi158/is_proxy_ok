83d9603b22bb4990a49ce3653221736a8fc6c5df
shengqi158 <shengqi158@163.com>
2016-06-01 21:43:48 -0400

    增加多线程支持

--- is_proxy_ok.py
+++ is_proxy_ok.py
@@ -15,8 +15,9 @@ import urllib2
 import optparse
 import re
 import bs4
+from multiprocessing.dummy import Pool as ThreadPool
 
-def get_proxys(file_name):
+def get_proxys(file_name, thread_num=5):
     """这里的文件内容可以是从cn-proxy.com复制过来的数据"""
     proxys = []
     ip_reg = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', re.I)
@@ -29,12 +30,20 @@ def get_proxys(file_name):
                         ip, port = line.strip().split()[0], line.strip().split()[1]
                         proxy = '%s:%s' %(ip, port)
                         print 'proxy',proxy
-                        if test_connection(proxy):
-                            proxys.append(proxy)
+#                        if test_connection(proxy):
+                        proxys.append(proxy)
+        pool = ThreadPool(thread_num)
+        results = pool.map(test_connection,proxys)
+        pool.close()
+        pool.join()
+        
+        proxys = list(set(results))
+        proxys = sorted(proxys)
+    
     except Exception,e:
         print 'error',e
 
-    return proxys
+    return proxys 
 
 def test_connection(proxy):
     url = 'http://www.baidu.com'
@@ -46,7 +55,7 @@ def test_connection(proxy):
     try:
         req = urllib2.Request(url,headers=headers)
         req.set_proxy(proxy, host_type)
-        response = urllib2.urlopen(req)
+        response = urllib2.urlopen(req,timeout=10)
         if response and response.getcode() == 200:
             data = response.read()
             soup = bs4.BeautifulSoup(data)
@@ -55,16 +64,16 @@ def test_connection(proxy):
                 name_baiduyixia = title[0].get_text()
                 search_obj = re.search(u'\u767e\u5ea6\u4e00\u4e0b',name_baiduyixia)
                 if search_obj:
-                    return True
+                    return proxy
         else:
-            return False
+            return ''
     except Exception,e:
         print 'get proxy:',proxy,e
-        return False
+        return ''
 
 
-def write_file(src, dst):
-    proxys = get_proxys(src)
+def write_file(src, dst, thread_num=5):
+    proxys = get_proxys(src,thread_num)
     print 'proxys',proxys
     if proxys:
         try:
@@ -81,12 +90,13 @@ def main():
     parse = optparse.OptionParser()
     parse.add_option('-s', '--src', dest='src_file', help='要测试的文件')
     parse.add_option('-d', '--dst', dest='dst_file', help='存取结果文件',default='proxy_ok.txt')
+    parse.add_option('-n', '--thread_num', dest='thread_num', help='线程数',default=5)
 #    parse.add_option('-h', '--help', dest='help', help='示例:python is_proxy_ok.py -s src_file -d dst_file')
     (options, args) = parse.parse_args()
     if not options.src_file :
         usage()
         sys.exit()
-    write_file(options.src_file, options.dst_file)
+    write_file(options.src_file, options.dst_file, options.thread_num)
 
 if __name__ == '__main__':
     main()
